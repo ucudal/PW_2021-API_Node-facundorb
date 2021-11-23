9371965ac4ae4f48e6a59263acc3040d89f3049b
facundorb <37991547+facundorb@users.noreply.github.com>
2021-11-23 01:03:22 -0300

    cambios en Json

--- src/app.js
+++ src/app.js
@@ -20,29 +20,22 @@ app.get('/hola-mundo', function(req, res) {
   res.send("¡Hola mundo!")
 });
 
+const respuesta = JSON.stringify({"experiencia-laboral": [{"empresa": "Integra CCS",
+"puesto": "Proyect Manager",
+"descripcion": "• Panificación, ejecución y validación de requerimientos de la plataforma.• Capacitación al cliente sobre el producto y/o nuevos requerimientos.• Relevamiento de tareas y seguimiento a miembros del equipo.• Elaboración de documentos de Alcance y documentación del proceso• Capacitación interna del departamento","fechaInicio": "07/01/2019",
+"fechaFin": "Actualidad"
+},
+{
+"empresa": "Antel",
+"puesto": "Pasante",
+"descripcion": "• Tareas Administrativas",
+"fechaInicio": "07/07/2018",
+"fechaFin": "05/01/2019"
+}]
+});
 
 app.get('/experiencia-laboral', function(req, res) {
-  res.send(`{
-    "experiencia-laboral": [
-      {
-        "empresa": "Integra CCS",
-        "puesto": "Proyect Manager",
-        "descripcion": "• Panificación, ejecución y validación de requerimientos de la plataforma.
-  • Capacitación al cliente sobre el producto y/o nuevos requerimientos.
-  • Relevamiento de tareas y seguimiento a miembros del equipo.
-  • Elaboración de documentos de Alcance y documentación del proceso
-  • Capacitación interna del departamento",
-        "fechaInicio": "07/01/2019",
-        "fechaFin": "Actualidad"
-      },
-       {
-        "empresa": "Antel",
-        "puesto": "Pasante",
-        "descripcion": "• Tareas Administrativas",
-        "fechaInicio": "07/07/2018",
-        "fechaFin": "05/01/2019"
-      }]
-  }`)
+  res.send(respuesta);
 });
 
 
